49153c36fc0c7495f290066bab37aeecfd77498e
[dyad] <bortolidiego@gmail.com>
2025-11-18 11:43:41 -0300

    [dyad] Used cache busting parameter for Supabase JS import in Edge Function. - wrote 8 file(s), added react-router-dom, sonner package(s) + extra files edited outside of Dyad

--- src/App.tsx
+++ src/App.tsx
@@ -1,2 +1,34 @@
-// Import statement in App.tsx 
-import ProfileCard from './components/ProfileCard';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { AppLayout } from './components/AppLayout';
+import { ProtectedRoute } from './components/ProtectedRoute';
+import { Toaster } from 'sonner';
+import { ThemeProvider } from './components/theme-provider';
+import KanbanBoard from './pages/KanbanBoard';
+import AuthPage from './pages/Auth';
+import BrainPage from './pages/Brain';
+import ChangelogPage from './pages/Changelog';
+import ProvisionWrapper from './components/ProvisionWrapper';
+
+function App() {
+  return (
+    <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
+      <BrowserRouter>
+        <Routes>
+          {/* Rotas Públicas */}
+          <Route path="/auth" element={<AuthPage />} />
+          <Route path="/provision" element={<ProvisionWrapper />} />
+          
+          {/* Rotas Protegidas */}
+          <Route element={<ProtectedRoute><AppLayout /></ProtectedRoute>}>
+            <Route path="/" element={<KanbanBoard />} />
+            <Route path="/brain" element={<ProtectedRoute requireAdmin><BrainPage /></ProtectedRoute>} />
+            <Route path="/changelog" element={<ChangelogPage />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+      <Toaster richColors />
+    </ThemeProvider>
+  );
+}
+
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -2,23 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
-import { Toaster } from './components/ui/sonner';
-import { ThemeProvider } from './components/theme-provider';
-import { ToastProvider } from './components/ui/toast';
-import { WorkspaceProvider } from './hooks/useWorkspace';
-import { UserRoleProvider } from './hooks/useUserRole';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
-      <ToastProvider>
-        <UserRoleProvider>
-          <WorkspaceProvider>
-            <App />
-            <Toaster />
-          </WorkspaceProvider>
-        </UserRoleProvider>
-      </ToastProvider>
-    </ThemeProvider>
+    <App />
   </React.StrictMode>,
 );
