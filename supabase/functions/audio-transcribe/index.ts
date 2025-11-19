0d8291e10796a8ecabd51010422874649b98e17f
[dyad] <bortolidiego@gmail.com>
2025-11-19 12:20:17 -0300

    [dyad] Fix 404 na Edge Function audio-transcribe - wrote 1 file(s) + extra files edited outside of Dyad

--- supabase/functions/audio-transcribe/index.ts
+++ supabase/functions/audio-transcribe/index.ts
@@ -1,5 +1,7 @@
-/* eslint-disable */ // @ts-nocheck
+/* eslint-disable */
+// @ts-nocheck
 import { serve } from "https://deno.land/std@0.190.0/http/server.ts";
+
 const corsHeaders = {
   "Access-Control-Allow-Origin": "*",
   "Access-Control-Allow-Headers": "authorization, x-client-info, apikey, content-type",
