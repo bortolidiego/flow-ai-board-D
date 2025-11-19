0f44281354514b28a8c4762b8706d4ce400f0e16
[dyad] <bortolidiego@gmail.com>
2025-11-19 14:58:33 -0300

    [dyad] Restaurando Sidebar e Filtros no App.tsx - wrote 1 file(s) + extra files edited outside of Dyad

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,15 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Index from "./pages/Index";
+import { AppLayout } from "./components/AppLayout";
+import { ProtectedRoute } from "./components/ProtectedRoute";
+import KanbanBoard from "./pages/KanbanBoard";
+import Auth from "./pages/Auth";
+import Brain from "./pages/Brain";
+import BrainNew from "./pages/BrainNew";
+import Changelog from "./pages/Changelog";
+import AcceptInvite from "./pages/AcceptInvite";
+import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
 
@@ -14,7 +22,56 @@ const App = () => (
       <Sonner />
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Index />} />
+          {/* Rotas Públicas */}
+          <Route path="/auth" element={<Auth />} />
+          <Route path="/accept-invite" element={<AcceptInvite />} />
+          
+          {/* Rotas Protegidas com Layout */}
+          <Route
+            path="/"
+            element={
+              <ProtectedRoute>
+                <AppLayout>
+                  <KanbanBoard />
+                </AppLayout>
+              </ProtectedRoute>
+            }
+          />
+          
+          <Route
+            path="/brain"
+            element={
+              <ProtectedRoute requireAdmin>
+                <AppLayout>
+                  <Brain />
+                </AppLayout>
+              </ProtectedRoute>
+            }
+          />
+
+          <Route
+            path="/brain/new"
+            element={
+              <ProtectedRoute requireAdmin>
+                <AppLayout>
+                  <BrainNew />
+                </AppLayout>
+              </ProtectedRoute>
+            }
+          />
+
+          <Route
+            path="/changelog"
+            element={
+              <ProtectedRoute>
+                <AppLayout>
+                  <Changelog />
+                </AppLayout>
+              </ProtectedRoute>
+            }
+          />
+
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
     </TooltipProvider>
